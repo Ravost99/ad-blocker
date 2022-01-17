defb47e327326a75f8af1848e72da4d2d9683232
Daniel Z <75264908+Ravost99@users.noreply.github.com>
2022-01-17 16:15:53 +0000

    Edit all files

---
+++ default_filters.js
@@ -0,0 +1,15 @@
+defaultFilters = [
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
+	"*://*.adservices.net/*",
+  "*://*yahoo.com/*"
+  "*ad*"
+];
